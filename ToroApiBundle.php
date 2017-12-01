27eace41ccd4503fb4a5757d3c0a021d5fb12103
phakpoom <im_bonbonn@hotmail.com>
2017-12-01 11:07:43 +0700

    Add php71 for sylius 1.0

--- ToroApiBundle.php
+++ ToroApiBundle.php
@@ -10,7 +10,7 @@ class ToroApiBundle extends AbstractResourceBundle
     /**
      * {@inheritdoc}
      */
-    public function getSupportedDrivers()
+    public function getSupportedDrivers(): array
     {
         return [
             SyliusResourceBundle::DRIVER_DOCTRINE_ORM,
@@ -20,7 +20,7 @@ class ToroApiBundle extends AbstractResourceBundle
     /**
      * {@inheritdoc}
      */
-    protected function getModelNamespace()
+    protected function getModelNamespace(): string
     {
         return 'Toro\Bundle\ApiBundle\Model';
     }
